50479f9d2ffeb56eded46557a7a8317ad2d91859
Christian James Welly <chrisjwelly@gmail.com>
2018-12-20 10:30:06 +0800

    Update test_matrices

--- test_matrices.js
+++ test_matrices.js
@@ -1,15 +1,58 @@
-const m1 = list(list(1,2,3),
-                list(4,5,6),
-                list(7,8,9));
+// Collection of test matrices
+// empty matrix
+const empty = [];
 
-const m2 = list(list(1,0,0),
-                list(0,1,1),
-                list(1,0,0));
+// 1x1 matrices:
+const m1_1x1 = list(list(0));
 
-const m3 = list(list(3,5,6),
-                list(2,7,8),
-                list(0,1,3));
+const m2_1x1 = list(list(1));
 
-const identity = list(list(1,0,0),
-                      list(0,1,0),
-                      list(0,0,1));
+// 2x2 matrices:
+const m1_2x2 = list(list(2,0),
+                    list(1,2));
+
+const m2_2x2 = list(list(1,8),
+                    list(8,1));
+
+const zero_2x2 = list(list(0,0),
+                      list(0,0));
+
+const identity_2x2 = list(list(1,0),
+                          list(0,1));
+
+// 3x3 matrices
+const m1_3x3 = list(list(1,2,3),
+                    list(4,5,6),
+                    list(7,8,9));
+
+const m2_3x3 = list(list(1,0,0),
+                    list(0,1,1),
+                    list(1,0,0));
+
+const m3_3x3 = list(list(3,5,6),
+                    list(2,7,8),
+                    list(0,1,3));
+
+const zero_3x3 = list(list(0,0,0),
+                      list(0,0,0),
+                      list(0,0,0));
+
+const identity_3x3 = list(list(1,0,0),
+                          list(0,1,0),
+                          list(0,0,1));
+
+// 2x3 matrices
+const m1_2x3 = list(list(2,1,8),
+                    list(3,5,6));
+
+const m2_2x3 = list(list(1,0,9),
+                    list(8,1,9));
+
+// 3x2 matrices
+const m1_3x2 = list(list(2,3),
+                    list(1,5),
+                    list(8,6));
+
+const m2_3x2 = list(list(1,8)
+                    list(0,1)
+                    list(9,9));
