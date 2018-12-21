4e73dd758f79e45cbee77348be5f5b2e3ee5788d
Christian James Welly <39089453+ChrisJWelly@users.noreply.github.com>
2018-12-21 17:21:31 +0700

    Update generate_vect.js

--- generate_vect.js
+++ generate_vect.js
@@ -1,10 +1,14 @@
-// using prompt to input
-function generate_vector(size) {
+// BETA FEATURE
+// A function that uses prompt to input the vector. Hassle-free! :D
+
+function gen_vector(size) {
     function iter(i, size, vect) {
         if (i === size) {
             return list(reverse(vect));
         } else {
-            const elem = parse_int(prompt("enter element " + (i + 1) ), 10);
+            const elem = parse_int(prompt("What you have so far: " + reverse(vect) + "\n" +
+                                          "Enter component " + (i + 1) + " out of " + size  + ":"
+                                          ), 10);
             return iter(i + 1, size, pair(elem, vect));
         }
     }
